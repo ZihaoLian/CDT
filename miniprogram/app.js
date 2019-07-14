78b18b236e9ef7550e957bae7204577ab1fe2ae2
biao <805650945@qq.com>
2019-07-14 17:21:07 +0800

    修复画钟储存上限问题

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,55 +1,49 @@
 //app.js
 //const worker = wx.createWorker('workers/request/index.js')
 App({
-  onLaunch: function () {    
-    wx.getSystemInfo({
-      success: e => {
-        this.globalData.StatusBar = e.statusBarHeight;
-        this.globalData.CustomBar = e.statusBarHeight + 46;
-        this.globalData.ContentHeight = e.screenHeight - this.globalData.CustomBar - 90;  
-        this.globalData.sclar = e.screenHeight / e.screenWidth;
-        this.globalData.screen_width = e.screenWidth;
-        this.globalData.screen_height =e.screenHeight;
-      }
-    })
-
-    wx.cloud.init({
-      traceUser: true,
-    })
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+    onLaunch: function() {
+        wx.getSystemInfo({
+            success: e => {
+                this.globalData.StatusBar = e.statusBarHeight;
+                this.globalData.CustomBar = e.statusBarHeight + 46;
+                this.globalData.ContentHeight = e.screenHeight - this.globalData.CustomBar - 90;
+                this.globalData.sclar = e.screenHeight / e.screenWidth;
+                this.globalData.screen_width = e.screenWidth;
+                this.globalData.screen_height = e.screenHeight;
             }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null,
-    userLoginNumber: 1
-  }
+        })
+
+        wx.cloud.init({
+            traceUser: true,
+        })
+        // 展示本地存储能力
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+
+
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            // 可以将 res 发送给后台解码出 unionId
+                            this.globalData.userInfo = res.userInfo
+
+                            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                            // 所以此处加入 callback 以防止这种情况
+                            if (this.userInfoReadyCallback) {
+                                this.userInfoReadyCallback(res)
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    },
+    globalData: {
+        userInfo: null,
+    }
 })
--- miniprogram/pages/test/test.js
+++ miniprogram/pages/test/test.js
@@ -1,462 +1,289 @@
 // pages/test/test.js
 const app = getApp();
+const CDTapi = require("../../api/CDTapi.js")
+
 var startX = 0;
 var startY = 0;
 var begin = false; //设置是否开始绘画
-var curDrawArr = []; //用来存储每个笔画的轨迹
-var timer; //计时器
-var time_num = 0;
-var t_num = 0;
-
-wx.cloud.init()
 
 Page({
     /**
      * 页面的初始数据
      */
     data: {
-      CustomBar: app.globalData.CustomBar/568 * app.globalData.sclar * 750,
-      StatusBar: app.globalData.StatusBar/568 * app.globalData.sclar * 750,
-      canvasHeight: 341/ 568 * app.globalData.sclar * 750,
-      filepath: wx.env.USER_DATA_PATH + '/',       
-      hour: 0, 
-      minute: 0,
-      step_tip: '请点击开始按钮进行测试',
-      filename: '' ,
-      showModal: false,
-      is_begin_draw: false,
-      showModalStatus: false,
-      is_empty : false,
-      is_next_step: false,
-      loginNumber: 1 //用户使用小程序进行测试次数
-    },
+        CustomBar: app.globalData.CustomBar / 568 * app.globalData.sclar * 750,
+        StatusBar: app.globalData.StatusBar / 568 * app.globalData.sclar * 750,
+        canvasHeight: 341 / 568 * app.globalData.sclar * 750,
+        hour: 0,
+        minute: 0,
+        step_tip: '请点击开始按钮进行测试',
+        showModal: false,
+        is_begin_draw: false,
+        showModalStatus: false,
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
+        tempArr: [],
+        drawArr1: [],
+        drawArr2: [],
+        image1: "",
+        image2: "",
+        secondStep: false
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
-      
+
+    onLoad: function() {
+        this.context = wx.createCanvasContext('canvas')
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
-      this.context = wx.createCanvasContext('firstcanvas'); //获取画布
-      this.fs = wx.getFileSystemManager(); //获取文件管理系统
-      this.db = wx.cloud.database(); //获取数据库系统
-      this.getOpen(); //获得用户的openid来自动命名文件
-      if(app.globalData.userLoginNumber > 1){
-        this.clear(); //在这里必须将画布画成白色，否则后面将画布转化为图片背景就会变成透明的
-      }
+    onShow() {
+        this.data.drawArr1 = []
+        this.data.drawArr2 = []
+        this.data.image1 = ""
+        this.data.image2 = ""
+        this.data.interval && clearInterval(this.data.interval); //关掉定时器
+
+        this.setData({
+            is_begin_draw: false, //防止下次进来后就直接进行画钟
+            step_tip: "请点击开始按钮进行测试",
+            secondStep: false
+        })
+
+        this.clear()
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    },
 
     // 开始绘制线条
     lineBegin: function(x, y) {
-      if(this.data.is_begin_draw){
-        begin = true;
-        this.context.beginPath();
-        startX = x;
-        startY = y;
-        this.lineAddPoint(x, y);
-      }
+        if (this.data.is_begin_draw) {
+            begin = true;
+            this.context.beginPath();
+            startX = x;
+            startY = y;
+            this.lineAddPoint(x, y);
+        }
     },
 
     // 绘制线条中间添加点
     lineAddPoint: function(x, y) {
-      this.context.moveTo(startX, startY)
-      this.context.lineTo(x, y);
-      this.context.stroke();
-      startX = x;
-      startY = y;
+        this.context.moveTo(startX, startY)
+        this.context.lineTo(x, y);
+        this.context.stroke();
+        startX = x;
+        startY = y;
     },
 
     // 绘制线条结束
     lineEnd: function() {
-      this.context.closePath();
-      begin = false;
+        this.context.closePath();
+        begin = false;
     },
 
     // 绘制开始 手指开始按到屏幕上
     touchStart: function(e) {
-      if(!this.data.is_begin_draw){ //必须先按开始按钮才能开始测试
-        this.tishiStart();
-      }
-      else{
-        this.setData({
-          is_next_step: true //放在这里防止用户不画钟就点击下一步
-        })
-        this.lineBegin(e.touches[0].x, e.touches[0].y)
-      }
+        if (!this.data.is_begin_draw) { //必须先按开始按钮才能开始测试
+            this.tishiStart();
+        } else {
+            this.lineBegin(e.touches[0].x, e.touches[0].y)
+        }
     },
-    
+
     // 绘制中 手指在屏幕上移动
     touchMove: function(e) {
-      if (begin) {
-          this.lineAddPoint(e.touches[0].x, e.touches[0].y);
-          this.context.draw(true); 
-      }
+        if (begin) {
+            this.lineAddPoint(e.touches[0].x, e.touches[0].y);
+            this.context.draw(true);
+        }
     },
 
     // 绘制结束 手指抬起
     touchEnd: function() {
-      curDrawArr.push({ //用来标记笔画的结束
-          x: -1,
-          y: -1,
-          t: -1
-      })
-      this.lineEnd();
+        if (begin) {
+            this.data.tempArr.push({ //用来标记笔画的结束
+                x: -1,
+                y: -1,
+                t: -1
+            })
+            this.lineEnd();
+        }
     },
 
-  //获得用户登陆次数
-  get_userLogin: function(){
-    this.db.collection('User_Login').doc('9c4488c75cbadbfe02d9c311504dbe57').update({
-      data: {
-        login_number : 34
-      },
-      success: console.log,
-      fail: console.error
-    })
-  },
 
-  //开始画钟
-  start_draw: function() {
-    this.context.setFillStyle('#ffffff');
-    this.context.fillRect(app.globalData.screen_width * (7 / 750), app.globalData.screen_heigh * (125 / 568), app.globalData.screen_width * 0.98, 341 / 568 * app.globalData.screen_height);
-    this.context.draw();
+    // 生成随机的文件名
+    genFileName() {
+        return (Math.random() * 10000000).toString(16).substr(0, 4) + Date.now().toString();
+    },
 
-    //设置上传路径 和 允许开始画钟
-    this.setData({
-      filepath: this.data.filepath + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc',
-      is_begin_draw: true,
-      is_empty: false
-    })
+    //开始画钟
+    start_draw: function() {
+        this.clear()
 
-    this.createNonceStr(); //随机生成时钟点数
-    record_XY(); //开始记录数据
-    this.get_userLogin();
-  },
-
-  //下一步进行复现画钟
-  next_step: function() {
-
-    if(this.data.is_next_step && !this.data.is_empty){ //防止没开始就点击下一步了
-      wx.showToast({
-        title: '请稍等片刻上传数据',
-        icon: 'loading'
-      })
-      this.store();
-      clearTimeout(timer); //关掉定时器
-      this.setData({
-        is_begin_draw: false, //防止下次进来后就直接进行画钟
-        is_next_step: false,
-        is_empty: true
-      })
-      //this.clear(); // 清楚数据，以备下次继续使用
-      wx.showToast({
-        title: '步骤一已完成',
-        duration: 1000,
-        success: function () {
-          wx.navigateTo({
-            url: '../test2/test2',
-          })
-        }
-      })
-    }
-    else{
-      this.void_withoutDraw();
-    }
-  },
-
-  void_withoutDraw:function(){
-    wx.showModal({
-      title: '提示',
-      content: '你还没开始画钟呢，请画完再继续下一步',
-      showCancel: false,
-      confirmColor: '#000000'
-    })
-  },
-
-  cancel_step: function(){
-    if(this.data.is_next_step){
-      wx.showModal({
-        title: '确定要取消吗?',
-        content: '取消后将重新进行测试!!!',
-        showCancel: false,
-        confirmColor: '#000000',
-        success(res) {
-          if (res.confirm) {
-          }
-        }
-      })
-      this.clear(); // 清空数组内容 
-      this.setData({
-        is_empty : true, //防止点击“取消”后点击“下一步”
-        is_begin_draw: false,
-        is_next_step: false
-      })
-      
-    }
-    else{
-      this.void_withoutDraw();
-    }
-  },
-
-  //随机生成时钟点数
-  createNonceStr: function () {
-    this.setData({
-      hour: Math.floor(Math.random() * 120 % 12),
-      minute: parseInt(Math.random() * 120 % 12)
-    })
-    while (Math.abs(this.data.hour - this.data.minute) < 3 || Math.abs(this.data.hour - this.data.minute)>4){ //不让时针和分针靠的太近
-      this.setData({
-        hour: Math.floor(Math.random() * 120 % 12),
-        minute: parseInt(Math.random() * 120 % 12)
-      })
-    }
-    this.setData({
-      hour: this.data.hour + 1,
-      minute: this.data.minute * 5,
-    })
-    this.setData({
-      step_tip: "请画出一个" + this.data.hour + "点" + this.data.minute + "分的时钟"
-    })
-  },
-
-  //获取用户的openid来自动命名文件
-  getOpen: function () {
-    wx.cloud.callFunction({
-      name: 'getUserOpenId',
-      complete: res => {
+        // 允许开始画钟
         this.setData({
-          filename: res['result']['openid']
+            is_begin_draw: true,
         })
-      }
-    })
-  },
 
-    //获取数据库里的登陆次数来刷新文件命名
-  //存储数据
-  store() {
-    this.record_clock(); //记录时钟点数
-    // this.fs.writeFileSync(this.data.filepath.toString() + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc', '-1 -1 -1\n', 'utf8')
-    console.log(this.data.filepath.toString() + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc')
-    this.fs.writeFileSync(this.data.filepath.toString(), '-1 -1 -1\n', 'utf8')
-    console.log(this.data.filepath.toString())
+        if (this.data.secondStep == false) {
+            this.createNonceStr(); //随机生成时钟点数、设置上传路径
+        }
 
-    for (var j in curDrawArr) {
-      // this.fs.appendFileSync(this.data.filepath.toString() + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc', curDrawArr[j].x.toString() + ' ' + curDrawArr[j].y.toString() + ' ' + curDrawArr[j].t.toString() + '\n', 'utf8')
-      this.fs.appendFileSync(this.data.filepath.toString(), curDrawArr[j].x.toString() + ' ' + curDrawArr[j].y.toString() + ' ' + curDrawArr[j].t.toString() + '\n', 'utf8')
-    }
+        this.record_XY(); //开始记录数据
+    },
 
-    //判断是否上传数据
-    if (!this.data.is_empty) {
-      this.save_data_draw(this.data.filename.toString()) //保存数据
-      this.save_first_draw(this.data.filename.toString()) //保存图片
-      this.delete_local_file();
-    }
-    else{
-      this.delete_clock();
-    }
-  },
+    //下一步进行复现画钟
+    next_step: function() {
+        if (this.data.tempArr.length != 0) { //防止没开始就点击下一步了
+            wx.showToast({
+                title: '步骤一已完成'
+            })
+            this.saveDraw('image1')
+            this.data.drawArr1 = this.data.tempArr
 
-  //删除缓存文件
-  delete_local_file: function(){
-    // var object = {
-    //   filepath: this.data.filepath.toString() + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc'
-    // }
-    // this.fs.removeSavedFile(object);
-    // wx.removeSavedFile({
-    //   filePath: this.data.filepath.toString() + this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString() + '_1.doc',
-    //   complete: function (res) {
-    //     console.log(res._id)
-    //   }
-    // })
-    //wx.clearStorage(object);
-    // console.log("市场年会")
-    wx.getSavedFileList({
-      success: function (res) {
-        console.log(res.fileList)
-      }
-    })
-    wx.removeSavedFile({
-      filePath: this.data.filepath.toString(),
-      success: function () {
-        console.log("删除了本地缓存")
-      }
-    })
-    
-  },
+            this.clear()
+            clearInterval(this.data.interval); //关掉定时器
 
-  //保存数据
-  save_data_draw:function(filename){
-    wx.cloud.uploadFile({
-      cloudPath: 'CDTData/' + filename + '_' + app.globalData.userLoginNumber.toString()+ '_1.doc',
-      filePath: this.data.filepath.toString() + filename + '_' + app.globalData.userLoginNumber.toString() + '_1.doc',
-      success: res => {
-        console.log("数据上传成功")
-      }
-    })
-  },
+            this.setData({
+                is_begin_draw: false, //防止下次进来后就直接进行画钟
+                step_tip: "步骤二：请复现你刚才画的时钟",
+                secondStep: true
+            })
+        } else {
+            this.void_withoutDraw();
+        }
+    },
+
+    finsh() {
+        let that = this
+        if (this.data.tempArr.length != 0) {
+            this.saveDraw('image2')
+            this.data.drawArr2 = this.data.tempArr
+            this.data.tempArr = []
+
+            this.clear()
+            clearInterval(this.data.interval); //关掉定时器
 
 
-  //保存最后绘制的图片
-  save_first_draw: function (filename) {
-    wx.canvasToTempFilePath({
-      x: app.globalData.screen_width * (7 / 750),
-      y: app.globalData.screen_heigh*(125/568),
-      width: app.globalData.screen_width * 0.98,
-      height: 341/ 568 * app.globalData.screen_height,
-      destWidth: 256,
-      destHeight: 256,
-      canvasId: 'firstcanvas',
-      success(res) {
-        //将截取的图片上传到云存储上
-        wx.cloud.uploadFile({
-          cloudPath: 'CDTImage/' + filename + '_'+ app.globalData.userLoginNumber.toString()+ '_1.png',
-          filePath: res.tempFilePath, // 文件路径
-          success: res => {
-            console.log("图片上传成功")
-          },
-          fail: err => {
-            // handle error
-            console.log("图片上传失败");
-          }
+        } else {
+            this.void_withoutDraw();
+        }
+    },
+
+    void_withoutDraw: function() {
+        wx.showModal({
+            title: '提示',
+            content: '你还没开始画钟呢，请画完再继续下一步',
+            showCancel: false,
+            confirmColor: '#000000'
         })
-      }
-    })
-  },
+    },
 
+    cancel_step: function() {
+        this.clear(); // 清空数组内容 
+    },
 
-  //清楚画布上的内容
-  clear:function(){
-    time_num = 0; //注意这个变量才是时间变化的关键
-    startX = 0;
-    startY = 0;
-    curDrawArr = []; //清空绘画笔迹
-    this.context.setFillStyle('#ffffff');
-    this.context.fillRect(app.globalData.screen_width * (5 / 750), app.globalData.screen_heigh * (125 / 568), app.globalData.screen_width * 0.98, 341 / 568 * app.globalData.screen_height);
-    this.context.draw();
-  },
-
-  //控制用户必须点击开始按钮才能进行测试
-  tishiStart: function () {
-    wx.showModal({
-      title: '提示',
-      content: '请先点击开始按钮再开始',
-      showCancel: false, 
-      confirmColor: '#000000',
-      success(res) {
-        if (res.confirm) {
-          console.log("用户点击确认")
+    //随机生成时钟点数
+    createNonceStr: function() {
+        this.setData({
+            hour: Math.floor(Math.random() * 120 % 12),
+            minute: parseInt(Math.random() * 120 % 12)
+        })
+        while (Math.abs(this.data.hour - this.data.minute) < 3 || Math.abs(this.data.hour - this.data.minute) > 4) { //不让时针和分针靠的太近
+            this.setData({
+                hour: Math.floor(Math.random() * 120 % 12),
+                minute: parseInt(Math.random() * 120 % 12)
+            })
         }
-      }
-    })
-  },
+        this.setData({
+            hour: this.data.hour + 1,
+            minute: this.data.minute * 5,
+        })
+        this.setData({
+            step_tip: "步骤一：请画出一个" + this.data.hour + "点" + this.data.minute + "分的时钟",
+            fileName: this.genFileName()
+        })
+    },
 
-  //记录用户画的时钟的点数
-  record_clock:function(){
-    this.db.collection('User_clock').add({
-      data: {
-        hour: this.data.hour,
-        minute: this.data.minute,
-        open_id: this.data.filename.toString() + '_' + app.globalData.userLoginNumber.toString()
-      },
-      success: res => {
-        // 在返回结果中会包含新创建的记录的 _id
-        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
-      },
-      fail: err => {
-        console.error('[数据库] [新增记录] 失败：', err)
-      }
-    })
-  },
+    //存储数据
+    store() {
+        CDTapi.storeClock(this.data.hour, this.data.minute, this.data.fileName, this.data.drawArr1, this.data.drawArr2, this.data.image1, this.data.image2).then(() => {
+            wx.hideLoading()
+        })
+    },
 
-  //删除用户画的点数
-  delete_clock: function(){
-    if (this.data.counterId) {
-      this.db.collection('User_clock').doc(this.data.counterId).remove({
-        success: res => {
-          wx.showToast({
-            title: '删除成功',
-          })
-          this.setData({
-            counterId: '',
-            hour: null,
-            minute: null,
-            open_id: null
-          })
-        },
-        fail: err => {
-          wx.showToast({
-            icon: 'none',
-            title: '删除失败',
-          })
-          console.error('[数据库] [删除记录] 失败：', err)
-        }
-      })
-    } else {
-      wx.showToast({
-        title: '无记录可删，请见创建一个记录',
-      })
+
+    //保存最后绘制的图片
+    saveDraw(which) {
+        let that = this
+        wx.canvasToTempFilePath({
+            x: app.globalData.screen_width * (7 / 750),
+            y: app.globalData.screen_heigh * (125 / 568),
+            width: app.globalData.screen_width * 0.98,
+            height: 341 / 568 * app.globalData.screen_height,
+            destWidth: 256,
+            destHeight: 256,
+            canvasId: 'canvas',
+            success(res) {
+                that.data[which] = res.tempFilePath
+                if (which == "image2") {
+                    that.store()
+                    wx.showModal({
+                        title: '提示',
+                        content: '你已经完成画钟测试了，是否再测试一次',
+                        cancelColor: "#000000",
+                        confirmColor: "#000000",
+                        success(res) {
+                            if (res.confirm) {
+                                that.setData({
+                                    is_begin_draw: false, //防止下次进来后就直接进行画钟
+                                    step_tip: "请点击开始按钮进行测试",
+                                    secondStep: false
+                                })
+                                that.clear()
+                            } else if (res.cancel) {
+                                wx.switchTab({
+                                    url: '../home/home',
+                                })
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    },
+
+
+    //清除画布上的内容
+    clear: function() {
+        startX = 0;
+        startY = 0;
+        this.data.tempArr = []; //清空绘画笔迹
+        this.context.setFillStyle('#ffffff');
+        this.context.fillRect(app.globalData.screen_width * (5 / 750), app.globalData.screen_heigh * (125 / 568), app.globalData.screen_width * 0.98, 341 / 568 * app.globalData.screen_height);
+        this.context.draw();
+    },
+
+    //控制用户必须点击开始按钮才能进行测试
+    tishiStart: function() {
+        wx.showModal({
+            title: '提示',
+            content: '请先点击开始按钮再开始',
+            showCancel: false,
+            confirmColor: '#000000'
+        })
+    },
+
+
+    //用来定时获取记录数据
+    record_XY() {
+        var t_num = 0
+        this.data.interval = setInterval(() => {
+            if ((startX != 0 || startY != 0) && begin) {
+                this.data.tempArr.push({ //每隔12ms记录一次
+                    x: startX,
+                    y: startY,
+                    t: t_num
+                });
+            }
+            t_num += 12
+        }, 12)
     }
-  }
 })
-
-//用来定时获取记录数据
-function record_XY() {
-  t_num = time_num * 12;
-  timer = setTimeout(function() {
-    if ((startX != 0 || startY != 0) && begin){
-      curDrawArr.push({ //每隔12ms记录一次
-        x: startX,
-        y: startY,
-        t: t_num
-      });
-    }
-    time_num = time_num + 1;
-    record_XY();
-  }, 12)
-}
-
